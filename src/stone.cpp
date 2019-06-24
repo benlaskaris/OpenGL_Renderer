1a4c46237240f6877d6b51a1ea50181ea0299141
Ben <benlaskaris@dhcp-wifi-8021x-155-41-71-131.bu.edu>
2019-06-24 10:52:15 -0400

    yeeeeee boi got that rectangle back - up next - actual stuff

--- src/stone.cpp
+++ src/stone.cpp
@@ -32,30 +32,25 @@ void Stone::draw() {
   // std::cout << "Drawing" << std::endl;
   glBindVertexArray(VAO);
   glEnableVertexAttribArray(0);  
-  // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // this is the line causing a bug 
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // this is the line causing a bug 
+  // glDrawArrays(GL_TRIANGLES, 0, 3);
 
-  // std::cout << glGetError() << std::endl; // returns 0 (no error)
 
 
 }
 
 void Stone::update() {
   glBindVertexArray(VAO);
-  // std::cout << glGetError() << std::endl; // returns 0 (no error)
-
   glBindBuffer(GL_ARRAY_BUFFER, VBO);  
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-  // std::cout << glGetError() << std::endl; // returns 0 (no error)
-
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float)* vertices.size(), vertices.data(), GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  // std::cout << glGetError() << std::endl; // returns 0 (no error)
-
   glEnableVertexAttribArray(0); 
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW); 
-  // std::cout << glGetError() << std::endl; // returns 0 (no error)
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float)* indices.size(), indices.data(), GL_STATIC_DRAW); 
+
+
+
 
    
 
